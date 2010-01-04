6bd2ca11f79d5d23b46b7cdfc979c4597dac9d4f
Stéphane Genaud <genaud@unistra.fr>
2010-01-04 19:43:45 +0000

    fork + tube

--- isi1/td/fork/tube.c
+++ isi1/td/fork/tube.c
@@ -1,3 +1,20 @@
+/**
+ * Demontre une communication entre deux processus a l'aide de pipe().
+ * Necessite la connaissance de fork().
+ * Le pere ecrit dans le pipe, le fils lit.
+ *
+ * pipe( int tube[2] ) cree une paire de file descriptors, tube[0] et tube[1],
+ * tube[0] est connecté à l'extrémité en lecture, tube[1] à l'extrémité en écriture.
+ *
+ *               open FD table
+ *              ------------------
+ *              | 0   |  stdin   |
+ *              |     |
+ *    tube[0]   | n   |  outpipe | <-+
+ *    tube[1]   | n+1 |  inpipe  | --+
+ *
+ **/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,7 +24,6 @@ int main() {
 	  int tube[2];
 
         /* creat pipe */ 
-	  ret = pipe( tube );
 	  if (ret == -1 ) {
 		   printf("[erreur]: echec pipe().\n");
 		   exit(1); 
@@ -16,13 +32,15 @@ int main() {
         /* creat fils */ 
 	  ret = fork();
 	  if (ret == -1 ) {
-		   printf("[erreur]: echec fork().\n");
-		   exit(1); 
+		    ret = pipe( tube );
+		    printf("[erreur]: echec fork().\n");
+		    exit(1); 
 	  }
 
 	  /* pere ecrit , fils lit */
 	  if (ret == 0 ) {
 		   char valeur;
+		   printf("[fils] tube[0][1]=[%d %d]\n", tube[0],tube[1] );
 		   printf("[fils] bloqué en lecture sur tube[0]=%d\n", tube[0] );
 		   close( tube[1] );
 		   ret = read( tube[0], &valeur, 1 );
@@ -31,6 +49,7 @@ int main() {
 	  }
 	  else {
 		   char valeur;
+		   printf("[pere] tube[0][1]=[%d %d]\n", tube[0],tube[1] );
 		   close( tube[0] );
 		   printf("[pere] saisir valeur ?\n");
 		   scanf("%c",&valeur);
---
+++ isi1/td/fork/tube_2prod.c
@@ -0,0 +1,85 @@
+/**
+ * Demontre une ecriture concurrente dans un pipe.
+ * Necessite la connaissance de fork().
+ * Le pere cree deux fils A et B qui ecrivent dans le tube, puis se met en lecture permanente
+ * du pipe.
+ *
+ * On observe les séquences de production et consommation.
+ *
+ **/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * producteur
+ **/
+void producteur( char id, int iter, int tube[]) {
+int ret;
+int i;
+	printf("[producteur %c] demarre.\n",id);
+	for (i=0;i<iter;i++) {
+		   ret = write( tube[1], &id, 1 );
+		   //printf("%d ",ret);
+	}
+	printf("[producteur %c] termine.\n",id);
+}
+
+
+
+/**
+ * main: pere cree 2 fils et devient consommateur
+ **/
+
+int main() {
+	  int ret;
+	  int tube[2];
+
+        /* creat pipe */ 
+        ret = pipe( tube );
+	  if (ret == -1 ) {
+		   printf("[erreur]: echec pipe().\n");
+		   exit(1); 
+	  }
+
+	  /* creat fils */ 
+	  ret = fork();
+	  if (ret == -1 ) {
+		    printf("[erreur]: echec fork().\n");
+		    exit(1); 
+	  }
+
+	  if (ret == 0 ) {  /* fils 1 = producteur 1*/
+		    producteur( 'A', 1000, tube );
+	  }
+	  else {
+		    /* creat fils */ 
+		    ret = fork();
+		    if (ret == -1 ) {
+				printf("[erreur]: echec fork().\n");
+				exit(1); 
+		    }
+
+		    /* pere ecrit , fils lit */
+		    if (ret == 0 ) {  /* fils 2 = producteur 2*/
+				producteur( 'B', 100 , tube );
+		    }
+		    else {
+				/* le pere = consommateur */
+				printf("[consommateur] demarre.\n");
+				while (1) {
+					  char lu;
+					  ret = read( tube[0], &lu, 1 );
+					  printf("%c",lu);
+					  fflush(stdout);
+				}
+
+		    }
+	  }
+
+}
+
+
