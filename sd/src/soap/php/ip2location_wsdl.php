f62ee37bcddc7275868389f6d38d4bd32ca5e2bd
Stéphane Genaud <genaud@unistra.fr>
2011-10-05 13:40:53 +0000

    few modifs

--- sd/src/soap/php/ip2location_wsdl.php
+++ sd/src/soap/php/ip2location_wsdl.php
@@ -7,17 +7,17 @@
 // free license obtained from fraudlab
 // 90 credits / month, ask one at :
 // http://www.fraudlabs.com/freelicense.aspx?PackageID=1
-$LicenseKey="02-Q24B-X57Z"; 
+$LicenseKey="02-WVGM-1BA6"; 
 
 
 $query="130.79.192.160";
 $wsdl_url = "http://ws.fraudlabs.com/ip2locationwebservice.asmx?wsdl";
    
    $c = new Soapclient($wsdl_url);
-   //print_r( $c->__getTypes());
+   print_r( $c->__getTypes());
    $SoapResponse = $c->IP2Location(array("IP" => $query, "LICENSE" => $LicenseKey));
    $result = $SoapResponse -> IP2LocationResult;
-   //print_r($result);
+   print_r($result);
    if (!isset($result->Message)) {
    	print("IP $query localisée en (lat,lon): ".$result->LATITUDE ."°,". $result->LONGITUDE."°\n");
 	print("Pays  : ".$result->COUNTRYNAME . "\n");
