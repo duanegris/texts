47044ac961d3b4211ddd4908aaa49560eedcfe50
Stéphane Genaud <genaud@unistra.fr>
2013-12-09 20:00:40 +0100

    a few modifs

--- sd/src/rmi/chat/Refresher.java
+++ sd/src/rmi/chat/Refresher.java
@@ -1,4 +1,6 @@
 /**
+ * A 
+ * S. Gznaud
  */
 
 import java.util.ArrayList;
@@ -8,6 +10,16 @@ public class Refresher extends Thread {
     private ChatService c;
     private int cli_id;
 
+  
+   /**
+    * Constructor
+    **/
+    public Refresher(ChatService c, int cli_id) {
+        this.c = c;
+        this.cli_id = cli_id;
+    }
+    
+    
     /**
      * display
      * Helper function in charge of output
@@ -16,20 +28,14 @@ public class Refresher extends Thread {
     public void display (ArrayList<Message> mlist) {
     	char ESC=27; //
     	String red="[0;31;40m";
+    	String underlinered="[3;31;40m";
     	String normal="[0;37;40m";
     	for (Message m : mlist) {
-    		System.out.print("-> User "+m.getCliId()+" says: "+ESC+red);
-    		System.out.print(m.getContent());
-    		System.out.println(ESC+normal);
-    	}
-    	
+    		if (m.getContent().length()>0)
+    			System.out.println("-> User "+m.getCliId()+" says: "+ESC+ underlinered + m.getContent()+ESC+normal);
+    	}	
     }
     
-    
-    public Refresher(ChatService c, int cli_id) {
-        this.c = c;
-        this.cli_id = cli_id;
-    }
     
     /**
      * The thread start
