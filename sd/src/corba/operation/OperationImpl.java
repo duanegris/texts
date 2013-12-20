9361099deb7e63721a22715f2c6b0a43de58516d
Stéphane Genaud <genaud@unistra.fr>
2013-12-20 12:29:35 +0100

    simplify base example operation

---
+++ sd/src/corba/operation/OperationImpl.java
@@ -0,0 +1,25 @@
+import SimpleComputation.*;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.CORBA.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POA;
+
+import java.util.Properties;
+
+class OperationImpl extends OperationPOA {
+  private ORB orb;
+  public void setORB(ORB orb_val) {
+    orb = orb_val; 
+  }
+    
+  // implement sayOperation() method
+  public int addition(int a, int b) {
+    return(a+b);
+  }
+    
+  // implement shutdown() method
+  public void shutdown() {
+    orb.shutdown(false);
+  }
+}
