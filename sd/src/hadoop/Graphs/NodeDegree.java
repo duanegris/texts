00ad5d6c34a20f94a2116273be8da7a85e062167
Stéphane Genaud <genaud@unistra.fr>
2014-11-26 19:18:10 +0100

    simplify second reducer

--- sd/src/hadoop/Graphs/NodeDegree.java
+++ sd/src/hadoop/Graphs/NodeDegree.java
@@ -1,5 +1,3 @@
-package org.myorg;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,9 +21,14 @@ public class NodeDegree {
 	public static class MapFirstStep extends
 			Mapper<LongWritable, Text, Text, Text> {
 
-		//First mapping
-		//The edges are split into nodes
-		//Each node is send to reduce (with the edge that it comes from as value)
+		/** 
+		 * First map.
+		 * The edges denoted as a pair of vertices (=an edge)  are split into nodes. Each node 
+             * is sent to the reducer together with the edge it belongs to. 
+		 * @param key is a line number in the input file
+ 		 * @param value is the line content : expected format is: (U,V) for vertices U and V
+		 * @return two (k,v) pairs are output: ("U","(U,V)") and ("V","(U,V)")
+		 **/
 		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {				
 			String[] nodes = value.toString().replace("(", "").replace(")","").split(",");
@@ -34,20 +37,28 @@ public class NodeDegree {
 		}
 	}
 
+
 	public static class ReduceFirstStep extends
 			Reducer<Text, Text, Text, Text> {
 
-		//First reduce
-		//Counts the number of times a node is encountered. It determines its degree.
-		//The degree is send back with its associated edge
+		/**
+		 * First reduce.
+		 * Counts the number of times a node is encountered. It determines its degree.
+		 * The degree is send back with its associated edge
+		 * @param key a vertex
+		 * @param values an iterator on the vertices pairs (=edges) associated to key
+		 * @return 
+		 **/
 		public void reduce(Text key, Iterable<Text> values,
 				Context context) throws IOException, InterruptedException {		
 			Integer degree = 0;
 			List<Text> valuesList = new ArrayList<Text>();
 			for(Text value : values){
+				// The Iterable over Text seems to consume values, so save them
 				valuesList.add(new Text(value.toString()));
 				degree++;
 			}
+			// Emit the sum of occurences (degree) found for each edge
 			for(Text value: valuesList){
 				context.write(value, new Text("d("+key+")="+degree));
 			}			
@@ -68,35 +79,20 @@ public class NodeDegree {
 	public static class ReduceSecondStep extends
 	Reducer<Text, Text, Text, Text> {
 
-		//Merge all the data so it is display without duplicates
+		/**
+		 * Second reduce.
+		 * Merge all the edges and display the two individual vertex's degree 
+		 * (i.e  d(U)=x) all in one line
+		 * @param key a edge (a vertices pair)
+		 * @param values the individual degrees : [d(U)=x, d(V)=y] 
+		 **/
 		public void reduce(Text key, Iterable<Text> values,
 				Context context) throws IOException, InterruptedException {	
-			//The value of each node
-			String value1 = null;
-			String value2 = null;
-			String[] nodes = key.toString().replace("(", "").replace(")","").split(",");
+			String degreesLine="\t";
 			for(Text value : values){
-				//Find the value for each node and eliminates duplicates
-				if(value1 == null){
-					value1 = value.toString();
-				}
-				else{
-					if(!value.equals(value1)){
-						value2 = value.toString();
-					}
-				}
+				degreesLine += value + "\t"; 
 			}
-			if(value2 == null){
-				value2 = "";
-			}
-			//Sets the value in the order of the associated edge 
-			if(!nodes[0].equals(value1.substring(2, value1.indexOf(")")))){
-				String tmp;
-				tmp = value2;
-				value2 = value1;
-				value1 = tmp;
-			}
-			context.write(key, new Text(value1 + "\t" + value2));
+			context.write(key, new Text(degreesLine));
 		}
 	}
 	
@@ -120,47 +116,44 @@ public class NodeDegree {
 		//Creates a MapReduce Job
 		Configuration conf = new Configuration();		
 				
-		Job jobFirstPart = new Job(conf, "minpath");
+		Job job1 = new Job(conf, "My NodeDegree phase1");
 
-		jobFirstPart.setOutputKeyClass(Text.class);
-		jobFirstPart.setOutputValueClass(Text.class);
+		job1.setOutputKeyClass(Text.class);
+		job1.setOutputValueClass(Text.class);
 
-		jobFirstPart.setMapperClass(MapFirstStep.class);
-		jobFirstPart.setReducerClass(ReduceFirstStep.class);
+		job1.setMapperClass(MapFirstStep.class);
+		job1.setReducerClass(ReduceFirstStep.class);
 
-		jobFirstPart.setInputFormatClass(TextInputFormat.class);
+		job1.setInputFormatClass(TextInputFormat.class);
 		
 		//Uses the custom output format
-		jobFirstPart.setOutputFormatClass(MyTextOutputFormat.class);
+		job1.setOutputFormatClass(MyTextOutputFormat.class);
 
-		FileInputFormat.addInputPath(jobFirstPart, new Path(args[0]));
-		FileOutputFormat.setOutputPath(jobFirstPart, new Path(args[1]));
+		FileInputFormat.addInputPath(job1, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job1, new Path(args[1]));
 		
-		//Run the first job
-		jobFirstPart.waitForCompletion(true);
+		//Tell the jobtracker to run the first job and wait for completion
+		job1.waitForCompletion(true);
 		
-		
-		Job jobSecondPart = new Job(conf, "minpath");
+		Job job2 = new Job(conf, "My NodeDegree phase 2");
 
-		jobSecondPart.setOutputKeyClass(Text.class);
-		jobSecondPart.setOutputValueClass(Text.class);
+		job2.setOutputKeyClass(Text.class);
+		job2.setOutputValueClass(Text.class);
 
-		jobSecondPart.setMapperClass(MapSecondStep.class);
-		jobSecondPart.setReducerClass(ReduceSecondStep.class);
+		job2.setMapperClass(MapSecondStep.class);
+		job2.setReducerClass(ReduceSecondStep.class);
 
-		jobSecondPart.setInputFormatClass(TextInputFormat.class);
+		job2.setInputFormatClass(TextInputFormat.class);
 		
-		//Uses the custom output format
-		jobSecondPart.setOutputFormatClass(MyTextOutputFormat.class);
+		//Uses the custom output format to override directory protection
+		job2.setOutputFormatClass(MyTextOutputFormat.class);
 
 		//Takes information from the previous job
-		FileInputFormat.addInputPaths(jobSecondPart, args[1]+"/"+"part-r-00000");
-		FileOutputFormat.setOutputPath(jobSecondPart, new Path(args[1]));
+		FileInputFormat.addInputPaths(job2, args[1]+"/"+"part-r-00000");
+		FileOutputFormat.setOutputPath(job2, new Path(args[1]));
 		
 		//Run the second job
-		jobSecondPart.waitForCompletion(true);
-		
-		
+		job2.waitForCompletion(true);
 		
 	}
 
