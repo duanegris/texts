9daac9f5c1ee04fa04cbd8043243adbc91fc9559
Stéphane Genaud <genaud@ensiie.fr>
2015-08-30 18:56:29 +0200

    demo of what is passed as Input

---
+++ sd/src/hadoop/split/Makefile-hadoop-2.x
@@ -0,0 +1,38 @@
+#
+# 
+# Makefile for Hadoop 2.x
+#
+# ------------------------------------------------------------------------------
+# Take care of setting the $JAVA_HOME and $HADOOP_CLASS in your .bashrcrc or equivalent,
+# as examplified below:
+#
+# On MacOSX Yosemite a convenient way of setting JAVA_HOME is 
+# export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+
+
+# export HADOOP_VERSION=2.4.0
+# export HADOOP_HOME=/usr/local/Cellar/hadoop/${HADOOP_VERSION}   # from HomeBrew
+# export PATH=$HADOOP_HOME/bin:$PATH
+# export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
+# ------------------------------------------------------------------------------
+
+
+PROJECT=SplitRead
+JAVACC=javac
+JAR=jar
+
+
+
+all: $(PROJECT).jar
+
+$(PROJECT).jar : $(PROJECT).class 
+	$(JAR) -cvf $@ $<
+	@echo "** JAR READY **"
+	@echo "-> Make sure hadoop can reach your $(PROJECT).jar file." 
+	@echo '   e.g if in the current directory, export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:.'
+	@echo "-> Run :"
+	@echo "     hadoop jar $@ $(PROJECT) input output"
+	@echo "(make sure you have 'input' and 'output' does not exist yet)"
+
+$(PROJECT).class : $(PROJECT).java 
+	hadoop com.sun.tools.javac.Main $< 
---
+++ sd/src/hadoop/split/SplitRead.java
@@ -0,0 +1,61 @@
+/**
+  * Demonstration of Hadoop way of reading input data
+  *
+  * S. Genaud
+  **/
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class SplitRead {
+
+  public static class ReadMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    
+	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+			InputSplit split = context.getInputSplit();
+			/* FileSplit derives from InputSplit : It is the file we are working on */
+			FileSplit fsplit = (FileSplit) split;
+			System.out.println("* Map spawned: working on data split from ");
+			System.out.println("  - file   : "+fsplit.getPath());
+			System.out.println("  - offset : "+fsplit.getStart());
+			System.out.println("  - length : "+fsplit.getLength());
+			System.out.println("  - (key,value)=("+key+","+value+")");
+	    }
+  }
+  
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    FileSystem fs = FileSystem.newInstance(conf);
+
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: prog <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "word count");
+    job.setJarByClass(SplitRead.class);
+    job.setMapperClass(ReadMapper.class);
+    job.setOutputKeyClass(Text.class);
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
